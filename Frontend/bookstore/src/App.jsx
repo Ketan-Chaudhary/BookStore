8e2df252f38dccecdb03a41e88bdb41fcfb5da65
Ketan Chaudhary <ketanchaudharypoonia@gmail.com>
2024-06-07 01:22:31 +0530

    Footer

--- Frontend/bookstore/src/App.jsx
+++ Frontend/bookstore/src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Banner from './Components/Banner/Banner'
+import Footer from './Components/Footer/Footer'
 
 const App = () => {
   return (
     <>
     <Navbar/>
     <Banner/>
-    
+    <Footer/>
     </>
   )
 }
